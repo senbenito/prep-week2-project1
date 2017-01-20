e19f780c3bdb7ba36f0b576a522263e371b15d73
senbenito <shannon.rivers@gmail.com>
2017-01-19 20:02:18 -0700

    halfway thru Q4
    calculated JS math - save before trying to label

--- main.js
+++ main.js
@@ -15,31 +15,32 @@ problem.
 
 
 
-// When the JS is connected properly, the following code will update the status message. 
+// When the JS is connected properly, the following code will update the status message.
 document.getElementById("q1").innerHTML = "JS Page Connected Properly!";
 document.getElementById("q1").classList.add("status-good");
 
 
 
-// 2. Declare a new variable called 'feelings', 
-//    Then store a string with a word that describes   
-//    how well you feel about this course material. 
-//    Then display that string within the HTML page. 
+// 2. Declare a new variable called 'feelings',
+//    Then store a string with a word that describes
+//    how well you feel about this course material.
+//    Then display that string within the HTML page.
 
-
-
+var feelings = "Excitedly overwhlemed!";
+document.getElementById("q2").innerHTML = feelings;
 
 
 
 
 // 3. Declare a new variable called 'name',
 //    store your name within it, and then
-//    create a 'message' variable with a greeting, 
+//    create a 'message' variable with a greeting,
 //    concat the message and name variables
 //    and then display it within the HTML page.
 
-
-
+var name = "Shannon",
+    message = "Hey there, ";
+document.getElementById("q3").innerHTML = message + name;
 
 
 
@@ -47,13 +48,14 @@ document.getElementById("q1").classList.add("status-good");
 // 4. Declare two variables with number values.
 //    Add, subtract, multiply and divide them.
 //    Display the values of each within the HTML page.
-//    Then label these values the best you can. 
+//    Then label these values the best you can.
 
-
-
-
-
-
+var firstNumber = 75,
+    secondNumber = 25
+document.getElementById("q4add").innerHTML = firstNumber + secondNumber;
+document.getElementById("q4subtract").innerHTML = firstNumber - secondNumber;
+document.getElementById("q4multiply").innerHTML = firstNumber * secondNumber;
+document.getElementById("q4divide").innerHTML = firstNumber / secondNumber;
 
 // 5. Declare two variables with string values.
 //    Create a third variable that concatenates the two strings
@@ -76,7 +78,7 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// 7. When a button is clicked, add content into the ID "q7". 
+// 7. When a button is clicked, add content into the ID "q7".
 
 
 
@@ -84,7 +86,7 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// 8. Collect a number value from the user and when a button is clicked, 
+// 8. Collect a number value from the user and when a button is clicked,
 //    then double user's answer and display that value into the HTML page.
 
 
@@ -95,7 +97,7 @@ document.getElementById("q1").classList.add("status-good");
 
 // 9. Set the variable 'num1' to 5.
 //    Set the variable 'num2' to 10.
-//    Test to see if num1 is greater than num2. 
+//    Test to see if num1 is greater than num2.
 //    If so, output to the HTML page: Yes, the value of num1 is greater than num2
 //    If not, output to the HTML page: No, the value of num1 is not greater than num2
 
@@ -105,12 +107,12 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// 10. Set the variable 'score' to a number between 1-100. 
+// 10. Set the variable 'score' to a number between 1-100.
 //    Create the following conditions:
 //    If the score is above 90, output to the HTML page "Great job".
 //    If the score is between 60 and 89, output to the HTML page "Nice work, keep practicing".
 //    If the score is below 59, output to the HTML page "Very bad: study more and retake the test".
-//    Test to make sure each condition is met properly. 
+//    Test to make sure each condition is met properly.
 
 
 
@@ -131,7 +133,7 @@ document.getElementById("q1").classList.add("status-good");
 
 // B. Create the formula to determine the area of a rectangle.
 //     The rectangle is: 2 inches by 3 inches
-//     Display the result in the HTML page. 
+//     Display the result in the HTML page.
 
 
 
@@ -146,7 +148,7 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// D. Have the user enter their name into a textfield. When they click a button, find the length (number of characters) of their name and display the result in the HTML page. 
+// D. Have the user enter their name into a textfield. When they click a button, find the length (number of characters) of their name and display the result in the HTML page.
 
 
 
@@ -154,7 +156,7 @@ document.getElementById("q1").classList.add("status-good");
 
 
 
-// E. Create a textfield to request a test score between 0-100 from the user. 
+// E. Create a textfield to request a test score between 0-100 from the user.
 //    If the score is above 90, display in the HTML a message to tell the user "Great job!".
 //    If the score is between 60 and 89, display a message of "Nice work, keep practicing.".
 //    If the score is below 59, display a message of "Very bad: study more and retake the test.".
@@ -166,11 +168,11 @@ document.getElementById("q1").classList.add("status-good");
 
 /* SANDBOX TRACK
 
-Solving all of these problems is a great step in the right direction, 
-but the next step is coming up with your own arrangements to solve 
+Solving all of these problems is a great step in the right direction,
+but the next step is coming up with your own arrangements to solve
 new problems. Practice creating your own problems to solve and their solutions -
-you can even challenge your classmates! 
- 
-Also, consider how you can add/remove CSS styles to create added presentation with the results. 
+you can even challenge your classmates!
+
+Also, consider how you can add/remove CSS styles to create added presentation with the results.
 
 */
